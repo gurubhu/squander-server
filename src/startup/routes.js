103ebfc39e72f0915c8d7eac6c84e4b8d698f4c8
prasag1 <guru.prasad@bsci.com>
2022-11-20 20:36:34 +0530

    Account routes added

--- src/startup/routes.js
+++ src/startup/routes.js
@@ -1,5 +1,7 @@
 const authRoutes = require('../routes/authRoutes');
+const accountRoutes = require('../routes/accountRoutes');
 
 module.exports = function(app){
     app.use('/api',authRoutes);
+    app.use('/api',accountRoutes);
 }
